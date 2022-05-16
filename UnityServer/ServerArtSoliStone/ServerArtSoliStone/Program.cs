70176515a83c9a83495bac7d85eb67fca5c381b7
gaetanroux <gaetan.roux@ynov.com>
2022-05-16 11:00:45 +0200

    fin

---
+++ UnityServer/ServerArtSoliStone/ServerArtSoliStone/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ServerArtSoliStone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+
+            Server.Start(2, 27834);
+
+            Console.ReadKey();
+        }
+    }
+}
